f5e7b8cc8c1b54d3fedd5a150afdd5d77a7d3fa7
cheonggil.bang <fqj102@gmail.com>
2020-05-23 13:11:17 +0900

    동적으로 ListView 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+
+void main()=> runApp(MyApp(
+  items:List<String>.generate(1000,(i) => "Item $i")
+));
+
 
 class MyApp extends StatelessWidget{
+  final List<String> items;
+  MyApp({Key key,@required this.items}):super(key:key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'welcome',
       home: Scaffold(
         appBar: new AppBar(title:new Text('ListVeiw Weget')),
-        body: Center (
-          child: Container(
-            height: 200.0,
-            child: new MyList(),
-          )
+        body: new ListView.builder(
+          itemCount:items.length,
+          itemBuilder: (context,index){
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
         )
       ),
     );
@@ -23,28 +32,17 @@ class MyApp extends StatelessWidget{
 class MyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        new Container(
-            width: 180,
-            color:Colors.deepPurple
-        ),
-        new Container(
-            width: 180,
-            color:Colors.red
-        ), new Container(
-            width: 180,
-            color:Colors.blue
-        ), new Container(
-            width: 180,
-            color:Colors.lightBlue
-        ),
-        new Container(
-            width: 180,
-            color:Colors.deepOrange
+      return MaterialApp(
+      title:'Flutter Demo',
+        home:Scaffold(
+          appBar:new AppBar(title: new Text('ListView Widget')),
+          body:Center(
+            child: Container(
+              height: 200.0,
+              child: MyList(),
+            ),
+          )
         )
-      ],
     );
   }
 
